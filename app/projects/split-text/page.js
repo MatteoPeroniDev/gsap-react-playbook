1e5eccb5fcb4cbc1e1ec78b74578317f13b4a85a
MatteoPeroni93 <matteoperoni93@gmail.com>
2024-09-03 12:50:56 +0200

    With split-type free

--- app/projects/split-text/page.js
+++ app/projects/split-text/page.js
@@ -2,17 +2,18 @@
 import React, { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { SplitText } from "gsap-trial/SplitText";
+/* import { SplitText } from "gsap-trial/SplitText"; */
 import "./page.css";
+import SplitType from "split-type";
 
-gsap.registerPlugin(SplitText);
+/* gsap.registerPlugin(SplitText); */
 
 function Page() {
   const container = useRef(null);
 
   useGSAP(
     () => {
-      const textSplitted = new SplitText("#title", {
+      /* const textSplitted = new SplitText("#title", {
         type: "chars",
       });
       let chars = textSplitted.chars;
@@ -27,6 +28,21 @@ function Page() {
           stagger: 0.1,
           delay: 0.2,
           duration: 0.3,
+        }
+      ); */
+
+      const title = new SplitType("#title");
+      gsap.fromTo(
+        ".char",
+        {
+          y: 100,
+          visibility: "visible", // This is necessary to prevent flickering
+        },
+        {
+          y: 0,
+          stagger: 0.1,
+          delay: 0.2,
+          duration: 0.3
         }
       );
     },
